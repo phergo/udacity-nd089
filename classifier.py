84744523f521326c81b3793fb6f69025750ab94a
Pablo E. Hernandez <phergo@hotmail.com>
2019-05-02 18:58:39 -0600

    Add Classifier() class template based on torch.nn.Module

---
+++ classifier.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# PROGRAMMER: Pablo E. Hernandez
+# DATE CREATED: 2019-05-16
+# REVISED DATE:
+# PURPOSE: Build a Neural Network to classify flower images.
+from torch import nn
+
+
+class Classifier(nn.Module):
+    """Custom Neural Network for Flower Image Classifier.
+    Udacity - AI Programming with Python Nanodegree Program (ND089) Final Project
+    """
+
+    def forward(self, features):
+        """Performs a forward pass on the Neural Network.
+        """
+        return self.forward(features)
